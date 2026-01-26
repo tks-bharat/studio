3f149c854cdd39f12692de28b995e7483742602f
Innovation in Education <tanmay.kr2004@gmail.com>
2026-01-26 11:21:41 +0000

    also add I am serving as Chief Advisor of FinTech Society and founded th

--- src/components/site-credits.tsx
+++ src/components/site-credits.tsx
@@ -6,7 +6,7 @@ export function SiteCredits() {
           <p className="text-muted-foreground text-sm mb-1 font-semibold">Site managed by</p>
           <h4 className="text-2xl font-bold text-foreground">Tanmay Kumar Shrivastava</h4>
           <p className="mt-2 text-muted-foreground max-w-prose">
-            A selected member of the faculty cohort for Stanford, a Neo4j Certified Professional, and a Microsoft Certified person who is pursuing Data Science & Artificial Intelligence.
+            A selected member of the faculty cohort for Stanford, a Neo4j Certified Professional, and a Microsoft Certified person who is pursuing Data Science & Artificial Intelligence. I am serving as Chief Advisor of FinTech Society and founded the society.
           </p>
         </div>
       </div>
