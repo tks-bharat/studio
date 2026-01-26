f0f8978e6b34ece35890058e18cdf3f7b35f11b9
Innovation in Education <tanmay.kr2004@gmail.com>
2026-01-26 11:51:45 +0000

    change to A selected member of the Stanford faculty cohort, a Neo4j Cert

--- src/components/site-credits.tsx
+++ src/components/site-credits.tsx
@@ -7,7 +7,7 @@ export function SiteCredits() {
           <p className="text-muted-foreground text-sm mb-1 font-semibold">Site managed by</p>
           <h4 className="text-2xl font-bold text-foreground">Tanmay Kumar Shrivastava</h4>
           <p className="mt-2 text-muted-foreground max-w-prose">
-            A selected member of the faculty cohort for Stanford, a Neo4j Certified Professional, and a Microsoft Certified person who is pursuing Data Science & Artificial Intelligence. I am serving as Chief Advisor of FinTech Society and founded the society and invited to go to India AI Summit 2026 by Govt. of India.
+            A selected member of the Stanford faculty cohort, a Neo4j Certified Professional, and Microsoft Certified, currently pursuing Data Science & Artificial Intelligence. I serve as the Chief Advisor and founding member of the FinTech Society, and have been invited by the Government of India to attend the India AI Summit 2026
           </p>
         </div>
       </div>
