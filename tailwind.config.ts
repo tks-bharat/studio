4a965c476fc93c23ce4a1380f0f1d93bd1574b4c
Innovation in Education <tanmay.kr2004@gmail.com>
2026-01-28 15:54:56 +0000

    make a blinking effect! (_for element div_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,16 @@ export default {
             height: '0',
           },
         },
+        'blink': {
+          '50%': {
+            opacity: '0.4'
+          }
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'blink': 'blink 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
